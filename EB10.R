44c0a5dd4f54ab06488529af14b5161b00ea51d7
Felicia <feliciafoetzsch@gmx.de>
2023-06-23 15:55:17 +0200

    neu

--- EB10.R
+++ EB10.R
@@ -1,16 +1,30 @@
 library(haven)
 library(tidyverse)
-library(dyplr)
+
+#### Datensatz einlesen ####
 
 EB10 <- read_sav("ZA5234_v2-0-1.sav")
 
 #### Datenbereinigung ####
 
+#Auswahl relevante Variablen 
 
 df_1 <- select(EB10,
-               v6, #Land
-               v206, #Zustimmung EU
-               v554, #BJ
-               v555, #gender
-               v556 #Alter  
+               v6, 
+               v206, 
+               v553,
+               v554,
+               v555,
+               v556   
+               ) 
+
+#Umbenennen der Variablen
+
+df_1 <- select(df_1,
+               Land = v6, 
+               ZustimmungEU = v206, 
+               Bildungsjahre = v553,
+               BJrecoded = v554,
+               Gender = v555,
+               Alter = v556
                ) 
