e87f091101ce39542845c755fc0fd65482cde143
ffelischa <135153908+ffelischa@users.noreply.github.com>
2023-06-23 10:20:25 +0200

    Add files via upload

---
+++ EB10.R
@@ -0,0 +1,15 @@
+library(haven)
+
+EB10 <- read_sav("ZA5234_v2-0-1.sav")
+
+#### Datenbereinigung ####
+
+#v206 Zustimmung EU Mitglied 
+
+#v554 Bildungsjahre (D8)
+
+#v555 gender (D10)
+
+#v556 Alter (D11)
+
+
