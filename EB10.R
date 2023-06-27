9b1a6880119309b85c1b0de1b6dd710d5d33c57b
HannahHoffner <77341881+HannahHoffner@users.noreply.github.com>
2023-06-27 15:58:59 +0200

    Bildungsjahre verändert und Kreuztabelle geplottet

--- EB10.R
+++ EB10.R
@@ -40,18 +40,56 @@ clean_data <- na.omit(df_1)
 
 #neue Variable: 
 #ZustimmungDichotom: good=1, bad&neither good nor bad=0, dk=löschen (??)
-# Erstellung einer neuen Variable "EinstellungDichotom" mit NAs als Standardwert
+## Erstellung einer neuen Variable "EinstellungDichotom" mit NAs als Standardwert
 clean_data$EinstellungDichotom <- NA
 
-# Umkodierung der Antwortkategorien und Zuordnung zu "EinstellungDichotom"
+## Umkodierung der Antwortkategorien und Zuordnung zu "EinstellungDichotom"
 clean_data$EinstellungDichotom[clean_data$Zustimmung_EU == 1] <- 1
 clean_data$EinstellungDichotom[clean_data$Zustimmung_EU %in% c(2, 3)] <- 0
 
-# Löschen der Fälle mit den Antworten 4 (dont know) und 9(inap)
+## Löschen der Fälle mit den Antworten 4 (dont know) und 9(inap)
 clean_data <- clean_data[!clean_data$Zustimmung_EU %in% c(4, 9), ]
 
-# Überprüfung der neuen Variable
+## Überprüfung der neuen Variable
 table(clean_data$EinstellungDichotom)
+
+#Bildung gruppiert neu kodiert:
+# Umkodierung der Antwortkategorien von BJ_recoded in clean_data
+clean_data$BJ_gruppiert <- clean_data$BJ_recoded
+
+# Löschen der Fälle mit den Antworten 97 und 98
+clean_data <- clean_data[!(clean_data$BJ_gruppiert %in% c(97, 98)), ]
+
+# Umkodierung der Antwortkategorie 11 zu 0
+clean_data$BJ_gruppiert[clean_data$BJ_gruppiert == 11] <- 0
+
+# Überprüfung der neuen Variable
+table(clean_data$BJ_gruppiert)
+
+#UV: Bildung, AV: Einstellung
+## Laden des Pakets für Datenvisualisierung
+library(ggplot2)
+
+## Erstellung der Kreuztabelle mit Absolutwerten
+cross_table <- table(clean_data$EinstellungDichotom, clean_data$BJ_gruppiert)
+cross_table
+
+#Erstellung der Kreuztabelle2 mit Prozentwerten
+cross_table2 <- prop.table(table(clean_data$EinstellungDichotom, clean_data$BJ_gruppiert), margin = 2) * 100
+cross_table2
+
+# Umwandeln der Kreuztabelle2 in einen Datenframe
+cross_table2_df <- as.data.frame(cross_table)
+
+# Plot der Verteilung
+ggplot(data = cross_table2_df, aes(x = Var2, y = Freq, fill = as.factor(Var1))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Bildungsjahre", y = "Prozentualer Anteil", fill = "EinstellungDichotom") +
+  scale_fill_manual(values = c("1" = "blue", "0" = "red")) +
+  theme_minimal()
+
+
+
 
 #Geschlecht auf 0 & 1
 
