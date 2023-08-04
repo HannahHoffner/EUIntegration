ee4c705e1f7e1abca3c4f2a9345b9fb015636045
HannahHoffner <77341881+HannahHoffner@users.noreply.github.com>
2023-08-04 10:24:49 +0200

    Mehrebenenanalyse nach Preprocessing-Datensatz auf DatenMEA.sav

---
+++ MEAnachPrePro.R
@@ -0,0 +1,15 @@
+###Mehrebenenanalyse
+
+library(haven)
+library(tidyverse)
+library(readr)
+library(dplyr)
+library(ggplot2)    #Diagramme
+library(lme4)       #Mehrebenenanlyse
+library(misty)      #Zentrierung
+library(stargazer)  #Output
+library(performance)#ICC
+library(dplyr)      #BIP quadrieren
+library(ordinal)    #3-geteilte AV MEA
+
+DatensatzGesamt <- read_sav("DatenMEA.sav")
